377ce8d1718bb9e7e843264c77e3c643e3602d59
Sereno Dominguez <sereno@colostate.edu>
2023-05-22 02:01:31 +0900

    read routes.csv and build routes with references to stops set

--- main.cc
+++ main.cc
@@ -4,19 +4,20 @@
 #include <set>
 #include <vector>
 #include <sstream>
+#include <regex>
 
 #include "stop.h"
+#include "route.h"
 
 using namespace std;
 
-// default constructor
+// ---- stop stuff -------
+// ctors
 Stop::Stop()
   : id(""), name(""), desc(""), lat(0), lon(0) {};
-
-// actual constructor
+Stop::Stop(string i) : id(i), name(""), desc(""), lat(0), lon(0) {};
 Stop::Stop(string i, string n, string d, string la, string lo)
   : id(i), name(n), desc(d), lat(stold(la)), lon(stold(lo)) {};
-
 // copy constructor
 Stop::Stop(const Stop& s) {
   id = s.id;
@@ -25,7 +26,6 @@ Stop::Stop(const Stop& s) {
   lat = s.lat;
   lon = s.lon;
 }
-
 // assignment operator
 Stop& Stop::operator=(const Stop& rhs) {
   id = rhs.id;
@@ -35,40 +35,73 @@ Stop& Stop::operator=(const Stop& rhs) {
   lon = rhs.lon;
   return *this;
 }
-
 // less than operator
-bool Stop::operator<(const Stop& rhs) const {
-  return id < rhs.id;
-}
-  
+bool Stop::operator<(const Stop& rhs) const { return id < rhs.id; }
+
+ 
 int main() {
-  // open stops.csv
+
+  cout.precision(20);
+
+  // open stops.csv, construct stops set
   ifstream ifs("stops.csv");
   if (!ifs) {
     cout << "Error opening stops.csv\n";
     return 1;
   }
-
   set<Stop> stops;
   string line;
+  // each line is stops.csv is of the form: id,name,desc,lat,lon
   // for each line in stops.csv, split line by commas, initialize stop, insert in stops set
   while (getline(ifs, line)) {
     stringstream ss(line);
     vector<string> args;
     string arg;
     while (getline(ss, arg, ',')) args.push_back(arg);
-    for (auto a : args) cout << a << '\n';
     stops.insert(Stop(args[0], args[2], args[1], args[3], args[4]));
   }
   ifs.close();
 
-  // print out stops
-  cout.precision(20);
-  int i = 0;
-  for (Stop s : stops){
-    cout << s << '\n';
-    ++i;
+  // each line in routes.csv is of the form:
+  // routename,monstart-end freq, tuestart-end freq, ... sunstart-end freq, stop1 stop1time, stop2 stop2time, ...
+  // open routes.csv, construct routes vector
+  ifs.open("routes.csv");
+  if (!ifs) {
+    cout << "Error opening routes.csv\n";
+    return 1;
   }
-  cout << "total: " << i << '\n';
+  vector<Route> routes;
+  while (getline(ifs, line)) {
+    cout << line << '\n';
+    stringstream ss(line);
+    string arg;
+    getline(ss, arg, ',');
+    Route r(arg); // initialize route with name
+    // add each day's schedule to scheds array
+    for (int d = 0; d < 7; ++d) {
+      getline(ss, arg, ',');
+      // arg is of the form: start-end freq, split and initialize day_sched accordingly
+      Route::day_sched sched(arg.substr(0, arg.find("-")),
+                      arg.substr(arg.find("-")+1, arg.find(" ")),
+                      arg.substr(arg.find(" ")+1));
+      r.scheds[d] = sched;
+    }
+    while (getline(ss, arg, ',')) {
+      if (arg != "" && arg != "\n") {
+        Stop query(arg.substr(0, arg.find(" "))); 
+        auto it = stops.find(query);
+        if (it == stops.end()) {
+          cout << "Error: " << " route " << r.name << " " << arg.substr(0, arg.find(" ")) << " not found" << '\n';
+          return 1;
+        }
+        r.route_stops.insert(Route::route_stop(*it, arg.substr(arg.find(" ")+1)));
+      }
+    }
+    routes.push_back(r);
+  }
+  ifs.close();
+
+  for (auto r : routes) cout << r << '\n';
+
   return 0;
 }
