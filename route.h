164ebc3eb25e099817de131d17867b9c167683bc
Sereno Dominguez <sereno@colostate.edu>
2023-05-22 21:16:03 +0900

    got rid of route_stop struct and implemented maps for indexing stops/times

--- route.h
+++ route.h
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <string>
 #include <set>
+#include <map>
 
 #include "stop.h"
 
@@ -24,24 +25,8 @@ class Route {
   };
   day_sched scheds[7];  // one for each day of the week
 
-  // individual route_stops to be stored in route_stops set
-  struct route_stop {
-    Stop stop;
-    int time;
-    // all the definitions required to use route_stop in a set
-    route_stop() : stop(Stop()), time(0) {};
-    route_stop(const Stop &s, std::string t) : stop(s), time(stoi(t)) {};
-    route_stop(const route_stop& rs) : stop(rs.stop), time(rs.time) {};
-    route_stop& operator=(const route_stop& rs) {
-      stop = rs.stop;
-      time = rs.time;
-      return *this;
-    }
-    bool operator<(const route_stop& rhs) const {
-      return time < rhs.time;
-    }
-  };
-  std::set<route_stop> route_stops;
+  std::map<int, Stop> time_sorted_stops;
+  std::map<Stop, int> id_sorted_stops;
 };
 
 std::ostream & operator<<(std::ostream &os, const Route &r) {
@@ -51,8 +36,8 @@ std::ostream & operator<<(std::ostream &os, const Route &r) {
   }
   os << "\n   ";
   int i = 0;
-  for (auto rs : r.route_stops) {
-    os << ++i << " " << rs.stop << " @ " << rs.time << "\n   ";
+  for (const auto &[t, s] : r.time_sorted_stops) {
+    os << ++i << " " << s << " @ " << t << "\n   ";
   }
   os << '\n';
   return os;
